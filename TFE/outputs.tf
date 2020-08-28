0bf44a8c3d39d8e190dd192f416b64d7f2fdaf52
Michael Ethridge <methridge@hashicorp.com>
2020-08-28 14:49:01 -0600

    Removing TFE Example

--- TFE/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "tfe" {
-  description = "TFE access values."
-  value = {
-    app_url          = "https://${var.subdomain}.${var.hostname}"
-    console_url      = "https://${var.subdomain}.${var.hostname}:8800"
-    lb_public_ip     = module.load-balancer.load_balancer_ip
-    console_password = module.configs.console_password
-  }
-}
